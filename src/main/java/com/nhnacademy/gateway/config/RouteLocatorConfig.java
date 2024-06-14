1164d19fe7f41405e00798346fe159fbe04c6747
okeio <dhdustn7414@gmail.com>
2024-06-14 13:07:19 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteLocatorConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("bookstore", p -> p.path("/bookstore").and().uri("lb://3RUNNER-BOOKSTORE"))
+                .route("coupon", p -> p.path("/coupon").and().uri("lb://3RUNNER-COUPON"))
+                .build();
+    }
+}
