66ec18e4511dfe66460787419aec66659bf4f0e8
okeio <dhdustn7414@gmail.com>
2024-07-01 15:34:20 +0900

    fix: RouteLocaterConfig -> yml file 분리

--- src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
+++ src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
@@ -5,17 +5,17 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class RouteLocatorConfig {
-
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("bookstore", p -> p.path("/**").and().uri("lb://3RUNNER-BOOKSTORE"))
-//                .route("coupon", p -> p.path("/coupon").and().uri("lb://3RUNNER-COUPON"))
-//                .route("auth", p -> p.path("/auth").and().uri("lb://3RUNNER-AUTH"))
-                .build();
-    }
-    //
-    //
-}
+// @Configuration
+// public class RouteLocatorConfig {
+//
+//     @Bean
+//     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//         return builder.routes()
+//                 .route("bookstore", p -> p.path("/**").and().uri("lb://3RUNNER-BOOKSTORE"))
+// //                .route("coupon", p -> p.path("/coupon").and().uri("lb://3RUNNER-COUPON"))
+// //                .route("auth", p -> p.path("/auth").and().uri("lb://3RUNNER-AUTH"))
+//                 .build();
+//     }
+//     //
+//     //
+// }
