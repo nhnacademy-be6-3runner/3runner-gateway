78ffad2d83570f6ce1b3e1d9800e6d91baab5c2a
administrator <administrator@administrators-Laptop.local>
2024-07-04 14:24:12 +0900

    hotfix:yml,main file fix

--- src/main/java/com/nhnacademy/gateway/Gateway.java
+++ src/main/java/com/nhnacademy/gateway/Gateway.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class Application {
+public class Gateway {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Gateway.class, args);
     }
 
 }
